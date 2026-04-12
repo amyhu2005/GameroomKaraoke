5c049bea5423d0975132f7074886ded826c9f60c
Amy Hu <amyhu@AmytekiMacBook-Air.local>
2026-04-12 11:22:04 -0700

    Make delete button more prominent (red button with text)

--- src/components/Waitlist.jsx
+++ src/components/Waitlist.jsx
@@ -190,7 +190,7 @@ export default function Waitlist({ waitlist, addToWaitlist, removeFromWaitlist, 
                         }
                       }}
                     >
-                      🗑️
+                      Delete
                     </button>
                   </>
                 )}
